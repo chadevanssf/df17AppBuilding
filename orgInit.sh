7741fde6261d148a9b952c93e55c1945ee636c2a
chadevanssf <chad.evans@salesforce.com>
2018-04-12 11:10:34 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx msm:org:create -f config/project-scratch-def.json -d 1 -s --json -u int -o work.shop -a vol
+sfdx msm:org:create -f config/project-scratch-def.json -d 1 -s --json -u int -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/masterImportPlan.json
